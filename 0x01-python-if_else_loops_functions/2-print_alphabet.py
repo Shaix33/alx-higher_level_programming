63966cf6415413ec55a46df1e13b3591ddd6a4e8
Shaix33 <KevinNdlondlo@gmail.com>
2022-12-02 03:14:32 -0800

    debugging

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
-num = 26
-
-str = " "
-x = ord('a') + num - 1
-y = ord('a')
-
-while (y <= x):
-    str += chr(y)
-    y += 1
-
-print(f"{str}")
+for x in range(97, 123):
+    print(chr(x), end = " ")
