606e9a974c3650c5f8b4bf086506cfa55639b00a
Shaix33 <KevinNdlondlo@gmail.com>
2022-12-20 01:15:27 -0800

    trial

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import add_0
+if __name__ == "__main__":
+    from add_0 import add
 
-a = 1
-b = 2
+    a = 1
+    b = 2
 
-print("{} + {}".format(a ,b)+" = {}".format(add_0.add(a,b)))
+    print("{} + {}".format(a ,b)+" = {}".format(add(a,b)))
