6008b9f293104cb4e8bc443d1c61738896f9ef6d
Shaix33 <KevinNdlondlo@gmail.com>
2022-12-28 02:03:45 -0800

    3. Infinite add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+
+    numArgs = len(sys.argv)
+    result = 0
+    args = sys.argv
+    for i in range(1, numArgs):
+        result = result + int(args[i])
+
+    print(result)
+        
+        
